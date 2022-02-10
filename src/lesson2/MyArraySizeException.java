6e5422f04ef1d90977935aede8edd8f8ce70857f
Alexandr <alex.arzumanyan20@gmail.com>
2022-02-10 13:57:49 +0400

    homework1

---
+++ src/lesson2/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package lesson2;
+
+public class MyArraySizeException extends IllegalArgumentException{
+    public MyArraySizeException() {
+        super("Неверный размер массива, должно быть 4х4");
+    }
+
+}
