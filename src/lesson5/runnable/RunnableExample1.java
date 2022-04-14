943d631f357e5fb0ca5315f2a61e203068710600
Alexandr <alex.arzumanyan20@gmail.com>
2022-04-14 13:33:48 +0400

    homework5 java2

---
+++ src/lesson5/runnable/RunnableExample1.java
@@ -0,0 +1,20 @@
+package lesson5.runnable;
+
+public class RunnableExample1 {
+    public static void main(String[] args) {
+//        final Thread thread = new Thread(new MyRunnable2(2000));
+//        thread.start();
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(500);
+                    System.out.printf("%s: %d%n", Thread.currentThread().getName(), i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+thread2.start();
+    }
+}
